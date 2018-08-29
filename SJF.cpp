211a8dc912ff90a993c142bf4386d23397000935
Shubh8992 <42221998+Shubh8992@users.noreply.github.com>
2018-08-30 02:12:13 +0530

    Update SJF.cpp

--- SJF.cpp
+++ SJF.cpp
@@ -1,40 +1,41 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main()
 {
-	int n,i,j,b[10],temp[10],index[10],t=0,w[10];
-	float total=0.0f;
-	cout<<"Enter the number of process : ";
+	int i,j,temp,b[10],n;
+	float tat=0,w=0,sum=0;
+	cout<<"Enter number of processes: ";
 	cin>>n;
-	
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
-			cout<<"Enter the burst time of process "<<i+1<<" : ";
-			cin>>b[i];
-			temp[i]=b[i];
+		cout<<"Burst time of "<<i<<" process: ";
+		cin>>b[i];
+	}
+
+	 for(i=1;i<=n;i++)
+	 {
+	 	for(j=1;j<=n;j++)
+	 	{
+	 		if(b[i]<b[j])
+	 		{
+	 		    temp=b[i];
+	 			b[i]=b[j];
+	 			b[j]=temp;
+			 }
+		 }
+	 }
+	cout<<b[2];
+	cout<<"\n\tProcess \t   BT    \t Waiting time \t turn around time\n";
+	for(i=1;i<=n;i++)
+	{	
+		tat=b[i]+w;
+		cout<<"Process   "<<i<<"  \t\t   "<<b[i]<<"  \t\t  "<<w<<"  \t\t "<<tat<<endl;
+		w+=b[i];
+		sum=sum+tat;
 	}
 	
-	sort(b,b+n);
-	for(i=0;i<n;i++)
-	{
-			for(j=0;j<n;j++)
-			{
-				if(b[i]==temp[j])
-				{
-					index[i]=j;
-				}	
-			}
-	}
-		for(i=0;i<n;i++)
-	{
-			w[index[i]]=t;
-			t+=b[i];
-			total+=w[index[i]];
-		}
-	//cout<<"\n\tProcess \t     Waiting time\n";
-		//	cout<<"\t   "<<i+1<<"\t\t\t "<<w[i]<<endl;
-	cout<<"\n\nTotal waiting time   : "<<total<<endl;
-	cout<<"\nAverage Waiting time : "<<total/n;
+	cout<<"Average waiting time : "<<tat/n<<endl;
+	cout<<"Average TAT :"<<sum/n;
 	return 0;
 }
 
